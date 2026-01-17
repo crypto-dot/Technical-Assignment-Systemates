b79ef9a8de8bd67a97405a1dc65accb1517bb7e8
Carlos <carlos2345532@gmail.com>
2026-01-17 15:26:38 -0600

    fix config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/coding_assignment/',
+  base: '/Technical-Assignment-Systemates/',
   plugins: [
     react({
       babel: {
